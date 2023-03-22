8cf79185a4ad4332462ae19054918463ad5a0eda
tladikholofelo <tladi.kholofelo@gmail.com>
2023-03-22 09:51:05 -0700

    Update models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
+    """Represents an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
     name = ""
+    place_amenities = relationship("Place", secondary=place_amenity)
