5d1d05897d041e6baf1c991de5537b78c890350c
tladikholofelo <tladi.kholofelo@gmail.com>
2023-04-23 09:48:20 -0700

    Add 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Prints 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Prints 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Prints C followed by the value of the text variable."""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """Prints Python, followed by the value of the text variable.
+    Usage: The default value of `text` = is cool."""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
